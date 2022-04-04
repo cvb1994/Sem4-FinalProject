fcdd9395da8c616f01eaebafb3a3d8417f3dbd99
Cao Vuong Bach <bachcvth1911012@fpt.edu.vn>
2022-04-04 13:57:27 +0700

    up

--- music_app/app/src/main/java/com/sem4/music_app/network/Common.java
+++ music_app/app/src/main/java/com/sem4/music_app/network/Common.java
@@ -1,7 +1,15 @@
 package com.sem4.music_app.network;
 
 public class Common {
+    /*
+    * For deploy running
+    */
     private static final String BASE_URL = "https://music-ner3yu5pda-as.a.run.app/";
+
+    /*
+     * For local running
+     */
+//    private static final String BASE_URL = "http://localhost:8080";
 
     public static ApiManager getAPI(){
         return RetrofitClient.getClient(BASE_URL).create(ApiManager.class);
